18cf9e11d0f7a1fc0b3df59e4e5b42812b83174f
davidwpacker <davidwgpacker@gmail.com>
2021-06-07 20:09:27 +1000

    css + variables added

---
+++ index.js
@@ -0,0 +1,10 @@
+// variables for game state
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
+
+// Create variables to store references to the necessary DOM nodes
+
+let x = document.getElementById("") // player 1 counter
+let y = document.getElementById("") // player 2 counter 
+let z = document.getElementById("") // Dice roll??
